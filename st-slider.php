627c8f1e0e3a8c7ae82f4b1af57226368876a9a4
sumit tiwari <sumit.tiwari.prdxn@gmail.com>
2017-06-29 18:18:32 +0530

    adding minify css and js

--- st-slider.php
+++ st-slider.php
@@ -3,7 +3,7 @@
  * Plugin Name: ST Slider
  * Description: Add slider on your site by using [st_slider] shortcode for more detail please see detail page.
  * Author: Sumit Tiwari
- * Version: 1.1.0
+ * Version: 1.0.0
  * License: GPLv2
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
@@ -22,10 +22,10 @@ require_once('short-code/st-slider-short-code.php');
 
 function stap_slider_scripts() {
 	//enqueue the style
-    wp_enqueue_style( 'stap-style', constant('STAP_PLUGIN_PATH') . 'assets/css/style.css');
+    wp_enqueue_style( 'stap-style', constant('STAP_PLUGIN_PATH') . 'assets/css/style.min.css');
 
     //enqueue the script
-    wp_enqueue_script( 'stap-script', constant('STAP_PLUGIN_PATH') . 'assets/js/script.js', array('jquery'), '0.0.1', true );
+    wp_enqueue_script( 'stap-script', constant('STAP_PLUGIN_PATH') . 'assets/js/script.min.js', array('jquery'), '0.0.1', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'stap_slider_scripts' );
